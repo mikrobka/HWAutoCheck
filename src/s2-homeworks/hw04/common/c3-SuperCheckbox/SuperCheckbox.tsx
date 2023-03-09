d119c23853c5db0f531b090c5fb2772af5e0cf1f
Andrey Paramonov <mikrobka231@gmail.com>
2023-03-09 13:30:17 +0300

    4hw

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,6 +27,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+            onChangeChecked?.(e.currentTarget.checked)
         // задачка на написание онченджа
 
     }
