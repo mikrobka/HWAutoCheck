087af826a0e5f4112ae76cf7cef46916b03ea57a
Andrey Paramonov <mikrobka231@gmail.com>
2023-04-11 17:08:44 +0300

    8hw

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -10,15 +10,14 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort':  // by name
-            if(action.payload === 'up'){
-              return  [...state].sort((a,b) => a.name.localeCompare(b.name))
+            switch (action.payload){
+                case 'up':
+                    return  [...state].sort((a,b) => a.name.localeCompare(b.name))
+                case 'down':
+                    return  [...state].sort((a,b) => b.name.localeCompare(a.name))
+                default:
+                    return state
             }
-            if(action.payload === 'down'){
-                return  [...state].sort((a,b) => b.name.localeCompare(a.name))
-            }else{
-                return state
-            }
-
         case 'check':
           return state.filter(a => a.age >= 18)
         default:
